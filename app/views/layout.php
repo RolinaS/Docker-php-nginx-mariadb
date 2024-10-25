e7f99df30da37c8bbafa3704d5be4e0cbcb16078
RolinaS <sami.rolina@outlook.fr>
2024-10-25 11:02:15 +0200

    ajout du header

--- app/views/layout.php
+++ app/views/layout.php
@@ -2,7 +2,10 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
-  <title>MVC</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>MVC</title>
 </head>
 <body>
     <?php require './views/partials/_header.php'; ?>
